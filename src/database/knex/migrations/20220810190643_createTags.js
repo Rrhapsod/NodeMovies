5717f56d1e2246036a3dd63376d509395033875f
Renan Rondon <renanrondon@live.com>
2022-08-12 15:15:46 -0300

    including movies and tags

--- src/database/knex/migrations/20220810190643_createTags.js
+++ src/database/knex/migrations/20220810190643_createTags.js
@@ -1,8 +1,8 @@
 exports.up = (knex) =>
   knex.schema.createTable("tags", (table) => {
     table.increments("id");
-    table.integer("note_id").references("id").inTable("movies");
-    table.integer("user_id").references("id").inTable("users");
+    table.integer("movie_id").references("id").inTable("movies").onDelete("CASCADE");
+    table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     table.text("name");
   });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const usersRoutes = require("../routes/users.routes");
+const moviesRoutes = require("./movies.routes");
+const tagsRoutes = require("./tags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
+routes.use("/movies", moviesRoutes);
+routes.use("/tags", tagsRoutes);
 
 module.exports = routes;
