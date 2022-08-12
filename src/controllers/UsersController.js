331a4e67310ae89644024d7912a212feaf6122fc
Renan Rondon <renanrondon@live.com>
2022-08-12 13:47:10 -0300

    implementing users update and delete

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,6 +22,49 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update(request, response){
+    const {name, email, password, old_password} = request.body
+    const {id} = request.params
+
+    const user = await knex("users").where({ id }).first();
+    if (!user) {
+      throw new Error("Usuário não encontrado!");
+    }
+    
+    const userWithUpdatedEmail = await knex("users").where({ email }).first();
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new Error("E-mail já cadastrado!");
+    }
+
+    if (password && !old_password) {
+      throw new Error("Informe a senha antiga para alterá-la!");
+    }
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+      if (!checkOldPassword) {
+        throw new Error("Senha incorreta!");
+      }
+      const hashedPassword = await hash(password, 8);
+
+      await knex("users").update({
+        name,
+        email,
+        password: hashedPassword,
+        updated_at: knex.fn.now()
+      }).where({id})
+
+      return response.status(201).json();
+    }
+  }
+
+  async delete (request, response){
+    const { id } = request.params;
+
+    await knex("users").where({ id }).delete();
+
+    return response.status(201).json();
+  }
 }
 
 module.exports = UsersController;
